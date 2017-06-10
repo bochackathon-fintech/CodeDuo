0f8154b17edcc4f3ebf0dc392b09a46e64d885e0
Andreas Angelides <da_mask80@hotmail.com>
2017-06-10 15:33:10 +0300

    v 0.3

---
+++ CodeDuo.MobileApi/Models/AcceptPayments.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeDuo.MobileApi.Models
+{
+    public class AcceptPayment
+    {
+        public string CanAccept { get; set; }
+    }
+}
